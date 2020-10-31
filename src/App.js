f8188ed105aac612991e314e50080ecf4578002d
kealymmb <kealymmb@gmail.com>
2020-10-31 11:05:49 +0200

    Implemented view work button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
-import Face from './screens/Face';
-import Box from '@material-ui/core/Box';
+import "./App.css";
+import Face from "./screens/Face";
+import Box from "@material-ui/core/Box";
 
 function App() {
   return (
     <div className="App">
       <Box>
-        <Face start={"Hi, I'm "} name={"Kealym Bromley"} end={"I am a React and React-Native developer."}/>
+        <Face
+          start={"Hi, I'm "}
+          name={"Kealym Bromley"}
+          end={"I am a React and React-Native developer."}
+        />
       </Box>
     </div>
   );
--- src/screens/Face.js
+++ src/screens/Face.js
@@ -1,34 +1,98 @@
-import './Face.css';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import "./Face.css";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import Typography from "@material-ui/core/Typography";
 
-const useStyles = makeStyles({
-    root: {
-      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-      border: 0,
-      borderRadius: 3,
-      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-      color: 'white',
-      height: 48,
-      padding: '0 30px',
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    minWidth: 300,
+    width: "100%",
+  },
+  image: {
+    position: "relative",
+    height: 60,
+    marginTop: 50,
+    color: "black",
+    "&:hover, &$focusVisible": {
+      zIndex: 1,
+      "& $imageBackdrop": {
+        opacity: 0.15,
+      },
+      "& $imageMarked": {
+        opacity: 0,
+      },
+      "& $imageTitle": {
+        border: "4px solid currentColor",
+      },
     },
-  });
+  },
+  focusVisible: {},
+  imageButton: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: theme.palette.common.white,
+  },
+  imageBackdrop: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.4,
+    transition: theme.transitions.create("opacity"),
+  },
+  imageTitle: {
+    position: "relative",
+    padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${
+      theme.spacing(1) + 6
+    }px`,
+    display: "inline-block",
+    fontSize: 25,
+    width: 180,
+  },
+}));
 
 function Face(props) {
+  const classes = useStyles();
   return (
     <div className="Face">
       <header className="App-header">
-          <div className="Lead">
-            {props.start}
-            <div className="My-Name">
-                {props.name}
-            </div>
-            .
-          </div>
-          {props.end}
-          <Button>
-              View My Work
-          </Button>
+        <div className="Lead">
+          {props.start}
+          <div className="My-Name">{props.name}</div>.
+        </div>
+        {props.end}
+        <div>
+          <ButtonBase
+            focusRipple
+            className={classes.image}
+            focusVisibleClassName={classes.focusVisible}
+            style={{
+              width: 245,
+              height: 80,
+            }}
+          >
+            <span className={classes.imageButton} className="btn-Text">
+              <Typography
+                component="span"
+                variant="subtitle1"
+                className={classes.imageTitle}
+              >
+                View My Work
+              </Typography>
+            </span>
+          </ButtonBase>
+        </div>
       </header>
     </div>
   );
