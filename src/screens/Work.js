848b519ce6ecec0fd3a31a60b70b77df69879e41
kealymmb <kealymmb@gmail.com>
2020-11-01 15:05:02 +0200

    Added slidshow package

--- src/screens/Work.js
+++ src/screens/Work.js
@@ -1,8 +1,26 @@
 import "./Work.css";
 import React from "react";
+import ReactPlayer from "react-player/file";
+import Box from "@material-ui/core/Box";
+import promo from "../videos/cropped.mp4";
 
 function Work(props) {
-  return <div className="Face" id="work"></div>;
+  return (
+    <div className="App-header" id="work">
+      <Box display="flex">
+        <Box></Box>
+        <ReactPlayer
+          url={promo}
+          playing={true}
+          loop={true}
+          width={200}
+          height={450}
+          volume={0}
+          muted={true}
+        />
+      </Box>
+    </div>
+  );
 }
 
 export default Work;
